9ccf3d8d49ae6228f00247d8a3ef9b2627ef451e
olubusoye pelumi <olubusoye37@gmail.com>
2024-09-15 23:31:52 -0700

    errors cleared

--- _printf.c
+++ _printf.c
@@ -19,7 +19,6 @@ int _printf(const char *format, ...)
 		va_end(args);
 		return (-1);
 	}
-	
 	va_start(args, format);
 
 	while (format && format[i])
@@ -39,6 +38,7 @@ int _printf(const char *format, ...)
 					count += _putchar('%');
 					break;
 				default:
+					count += _putchar('%');
 					count += _putchar(format[i]);
 					break;
 			}
